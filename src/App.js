f56777e1f5aa3354622d914463842f82f5e590f4
黃汶鈞 <usj0326@gmail.com>
2021-06-13 02:29:42 +0800

    1. Add material-ui/icons, lodash, and react-router-dom modules
    2. Add page router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import HeroList from './HeroList'
+import HeroProfile from './HeroProfile'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <HeroList />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Redirect exact from="/" to="/heroes" />
+          <Route path="/heroes" exact>
+            <HeroList />
+          </Route>
+          <Route path="/heroes/:heroId">
+            <HeroList />
+            <HeroProfile />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
