6c97bc099f8aa50dcac4012fa97972c3b9e86c67
黃汶鈞 <usj0326@gmail.com>
2021-06-16 11:54:07 +0800

    Modify not found and system error page styles, and App styles

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -1,5 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
+import NotFoundSvg from '../images/404.svg'
+import backgroundColor from '../theme'
 
 export default function NotFound() {
-  return <h1>很抱歉, 我們找不到這一頁。</h1>
+  const StyledNotFoundDiv = styled.div`
+    position: absolute;
+    background-color: white;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  `
+
+  const StyledSVG = styled.img`
+    width: 50%;
+    margin: 0 auto;
+  `
+
+  const StyledH2 = styled.h2`
+    font-size: 28px;
+    color: ${backgroundColor};
+  `
+
+  return (
+    <StyledNotFoundDiv variant="primary">
+      <StyledSVG src={NotFoundSvg} alt="404 page not found." />
+      <StyledH2 variant="default">抱歉, 我們找不到這一頁...</StyledH2>
+    </StyledNotFoundDiv>
+  )
 }
--- src/SystemError/SystemError.js
+++ src/SystemError/SystemError.js
@@ -1,5 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
+import SystemErrorSVG from '../images/500.svg'
+import backgroundColor from '../theme'
 
 export default function SystemError() {
-  return <h1>系統錯誤！</h1>
+  const StyledSystemErrorDiv = styled.div`
+    position: absolute;
+    background-color: white;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  `
+
+  const StyledSVG = styled.img`
+    width: 50%;
+    margin: 0 auto;
+  `
+
+  const StyledH2 = styled.h2`
+    font-size: 28px;
+    color: ${backgroundColor};
+  `
+
+  return (
+    <StyledSystemErrorDiv variant="primary">
+      <StyledSVG src={SystemErrorSVG} alt="500 system error." />
+      <StyledH2 variant="default">500, 系統錯誤！</StyledH2>
+    </StyledSystemErrorDiv>
+  )
 }
